95471927378cc75e30e30d6b0add35d91b4813ef
vivek chauhan <chauhan.vivek99@outlook.com>
2019-01-12 12:18:56 +0530

    ThirdPary Component

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
 import { AuthService } from "../_services/auth.service";
+import { AlertifyService } from "../_services/alertify.service";
 
 @Component({
   selector: "app-register",
@@ -9,17 +10,17 @@ import { AuthService } from "../_services/auth.service";
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
-  constructor(private auth: AuthService) {}
+  constructor(private auth: AuthService,private alertify:AlertifyService) {}
 
   ngOnInit() {}
 
   register() {
     this.auth.register(this.model).subscribe(
       () => {
-        console.log("User Created");
+        this.alertify.success("User Created");
       },
       error => {
-        console.log(error);
+       this.alertify.error(error);
       }
     );
   }
